9012440fd12cd66d9049cdcdb864c7360767015c
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-22 14:29:56 +0530

    FEAT : add Quantity Service Interface

--- QuantityMeasurement/src/service/QuantityService.java
+++ QuantityMeasurement/src/service/QuantityService.java
@@ -1,9 +1,14 @@
 package service;
 
 
+import model.LengthUnit;
 import model.QuantityLength;
 
 public interface QuantityService {
+
+    double convert(double value, LengthUnit from, LengthUnit to);
+
+    QuantityLength convert(QuantityLength quantity, LengthUnit to);
 
     boolean compare(QuantityLength q1, QuantityLength q2);
 }
