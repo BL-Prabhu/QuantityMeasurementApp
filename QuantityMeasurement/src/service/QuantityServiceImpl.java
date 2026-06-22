d0943f24a035032bd782cacfcdae171cd2fc01aa
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-22 12:58:52 +0530

    FEAT : add Quantity Service Implementation

--- QuantityMeasurement/src/service/QuantityServiceImpl.java
+++ QuantityMeasurement/src/service/QuantityServiceImpl.java
@@ -6,10 +6,12 @@ import model.QuantityLength;
 public class QuantityServiceImpl implements QuantityService {
 
     @Override
-    public boolean areEqual(QuantityLength q1, QuantityLength q2) {
+    public boolean compare(QuantityLength q1, QuantityLength q2) {
+
         if (q1 == null || q2 == null) {
             return false;
         }
-        return q1.equals(q2);
+
+        return q1.isEqual(q2);
     }
 }
