69194a094f4f5595981694d89e154089fdb72877
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-22 12:36:28 +0530

    FEAT : add Quantity Lenght Model

---
+++ QuantityMeasurement/src/model/QuantityLength.java
@@ -0,0 +1,42 @@
+package model;
+
+public class QuantityLength {
+
+    private final double value;
+    private final LengthUnit unit;
+
+    public QuantityLength(double value, LengthUnit unit) {
+        if (unit == null) {
+            throw new IllegalArgumentException("Unit cannot be null");
+        }
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public double toFeet() {
+        return value * unit.getConversionFactor();
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public LengthUnit getUnit() {
+        return unit;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        QuantityLength other = (QuantityLength) obj;
+
+        return Double.compare(this.toFeet(), other.toFeet()) == 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Quantity(" + value + ", " + unit + ")";
+    }
+}
