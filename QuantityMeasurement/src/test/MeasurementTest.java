b5d8fbf5bef18c0351126c55e0178ddbd6accc08
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-22 17:25:44 +0530

    FEAT : add Measurement Test

--- QuantityMeasurement/src/test/MeasurementTest.java
+++ QuantityMeasurement/src/test/MeasurementTest.java
@@ -1,6 +1,11 @@
 package test;
 
+import model.Feet;
+import model.Inches;
+import service.MeasurementService;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MeasurementTest {
 
