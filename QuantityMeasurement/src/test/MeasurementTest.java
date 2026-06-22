9891daa2174b49c05dddebd972efa460f6a8d69a
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-22 12:23:12 +0530

    FEAT : add Measurement Test

--- QuantityMeasurement/src/test/MeasurementTest.java
+++ QuantityMeasurement/src/test/MeasurementTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import model.Feet;
+import model.Inches;
 import service.MeasurementService;
 import org.junit.jupiter.api.Test;
 
@@ -10,21 +11,61 @@ public class MeasurementTest {
 
     MeasurementService service = new MeasurementService();
 
+    // Feet Tests
     @Test
-    void givenSameFeetValues_whenCompared_thenShouldReturnTrue() {
-
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
-
-        assertTrue(service.areEqual(f1, f2));
+    void testFeetSameValue() {
+        assertTrue(service.compareFeet(new Feet(1.0), new Feet(1.0)));
     }
 
     @Test
-    void givenDifferentFeetValues_whenCompared_thenShouldReturnFalse() {
+    void testFeetDifferentValue() {
+        assertFalse(service.compareFeet(new Feet(1.0), new Feet(2.0)));
+    }
 
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(2.0);
+    @Test
+    void testFeetNullComparison() {
+        Feet f = new Feet(1.0);
+        assertFalse(f.equals(null));
+    }
 
-        assertFalse(service.areEqual(f1, f2));
+    @Test
+    void testFeetDifferentType() {
+        Feet f = new Feet(1.0);
+        assertFalse(f.equals("ABC"));
+    }
+
+    @Test
+    void testFeetSameReference() {
+        Feet f = new Feet(1.0);
+        assertTrue(f.equals(f));
+    }
+
+    // Inches Tests
+    @Test
+    void testInchesSameValue() {
+        assertTrue(service.compareInches(new Inches(1.0), new Inches(1.0)));
+    }
+
+    @Test
+    void testInchesDifferentValue() {
+        assertFalse(service.compareInches(new Inches(1.0), new Inches(2.0)));
+    }
+
+    @Test
+    void testInchesNullComparison() {
+        Inches i = new Inches(1.0);
+        assertFalse(i.equals(null));
+    }
+
+    @Test
+    void testInchesDifferentType() {
+        Inches i = new Inches(1.0);
+        assertFalse(i.equals("ABC"));
+    }
+
+    @Test
+    void testInchesSameReference() {
+        Inches i = new Inches(1.0);
+        assertTrue(i.equals(i));
     }
 }
