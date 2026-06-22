41e1bb2459da3e037590a5f51a3e40f33e82f046
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-22 14:31:28 +0530

    FEAT : add Quantity Service Test

--- QuantityMeasurement/src/test/QuantityServiceTest.java
+++ QuantityMeasurement/src/test/QuantityServiceTest.java
@@ -1,7 +1,6 @@
 package test;
 
 
-
 import model.LengthUnit;
 import model.QuantityLength;
 import org.junit.Test;
@@ -13,50 +12,66 @@ import static org.junit.Assert.*;
 public class QuantityServiceTest {
 
     private final QuantityService service = new QuantityServiceImpl();
+    private static final double EPSILON = 0.0001;
 
     @Test
-    public void testYardToFeet() {
-        assertTrue(service.compare(
-                new QuantityLength(1, LengthUnit.YARDS),
-                new QuantityLength(3, LengthUnit.FEET)
-        ));
+    public void testFeetToInches() {
+        assertEquals(12.0,
+                service.convert(1, LengthUnit.FEET, LengthUnit.INCHES),
+                EPSILON);
     }
 
     @Test
-    public void testYardToInches() {
-        assertTrue(service.compare(
-                new QuantityLength(1, LengthUnit.YARDS),
-                new QuantityLength(36, LengthUnit.INCHES)
-        ));
+    public void testInchesToFeet() {
+        assertEquals(2.0,
+                service.convert(24, LengthUnit.INCHES, LengthUnit.FEET),
+                EPSILON);
+    }
+
+    @Test
+    public void testYardToFeet() {
+        assertEquals(3.0,
+                service.convert(1, LengthUnit.YARDS, LengthUnit.FEET),
+                EPSILON);
     }
 
     @Test
     public void testCmToInches() {
-        assertTrue(service.compare(
-                new QuantityLength(1, LengthUnit.CENTIMETERS),
-                new QuantityLength(0.393701, LengthUnit.INCHES)
-        ));
+        assertEquals(0.393701,
+                service.convert(1, LengthUnit.CENTIMETERS, LengthUnit.INCHES),
+                EPSILON);
     }
 
     @Test
-    public void testDifferentValues() {
-        assertFalse(service.compare(
-                new QuantityLength(1, LengthUnit.FEET),
-                new QuantityLength(2, LengthUnit.FEET)
-        ));
+    public void testObjectConversion() {
+        QuantityLength q = new QuantityLength(36, LengthUnit.INCHES);
+        QuantityLength result = service.convert(q, LengthUnit.YARDS);
+
+        assertTrue(result.isEqual(new QuantityLength(1, LengthUnit.YARDS)));
     }
 
     @Test
-    public void testNullHandling() {
-        assertFalse(service.compare(null,
-                new QuantityLength(1, LengthUnit.FEET)));
+    public void testRoundTrip() {
+        double val = service.convert(5, LengthUnit.FEET, LengthUnit.INCHES);
+        double result = service.convert(val, LengthUnit.INCHES, LengthUnit.FEET);
+
+        assertEquals(5.0, result, EPSILON);
     }
 
     @Test
+    public void testNegative() {
+        assertEquals(-12.0,
+                service.convert(-1, LengthUnit.FEET, LengthUnit.INCHES),
+                EPSILON);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void testInvalidUnit() {
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> new QuantityLength(1, null)
-        );
+        service.convert(1, null, LengthUnit.FEET);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidValue() {
+        service.convert(Double.NaN, LengthUnit.FEET, LengthUnit.INCHES);
     }
 }
