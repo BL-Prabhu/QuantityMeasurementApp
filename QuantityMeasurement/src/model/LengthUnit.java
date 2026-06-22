667342892ef629eda5fee4065b2c12ee3b09f98d
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-22 12:57:09 +0530

    FEAT : add Lenght Unit Model

--- QuantityMeasurement/src/model/LengthUnit.java
+++ QuantityMeasurement/src/model/LengthUnit.java
@@ -3,7 +3,9 @@ package model;
 public enum LengthUnit {
 
     FEET(1.0),
-    INCH(1.0 / 12.0);
+    INCHES(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(0.0328084);
 
     private final double conversionFactor;
 
@@ -11,7 +13,7 @@ public enum LengthUnit {
         this.conversionFactor = conversionFactor;
     }
 
-    public double getConversionFactor() {
-        return conversionFactor;
+    public double toFeet(double value) {
+        return value * conversionFactor;
     }
 }
