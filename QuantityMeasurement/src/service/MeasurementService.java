7735dfe118a4253756d6173717f65ec6e094d232
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-22 12:21:41 +0530

    FEAT : add Measurement Service

--- QuantityMeasurement/src/service/MeasurementService.java
+++ QuantityMeasurement/src/service/MeasurementService.java
@@ -1,11 +1,15 @@
 package service;
 
 import model.Feet;
+import model.Inches;
 
 public class MeasurementService {
 
-    // Business logic
-    public boolean areEqual(Feet f1, Feet f2) {
+    public boolean compareFeet(Feet f1, Feet f2) {
         return f1.equals(f2);
+    }
+
+    public boolean compareInches(Inches i1, Inches i2) {
+        return i1.equals(i2);
     }
 }
