97341aa458f02f85f8055654bd26cc173f48f19b
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-23 11:44:38 +0530

    FEAT : add Quantity Service Implementation

--- QuantityMeasurement/src/service/QuantityServiceImpl.java
+++ QuantityMeasurement/src/service/QuantityServiceImpl.java
@@ -1,27 +1,45 @@
 package service;
 
-import model.LengthUnit;
+
+import model.IMeasurable;
 import model.Quantity;
+
+import java.util.Objects;
 
 public class QuantityServiceImpl implements QuantityService {
 
     @Override
-    public Quantity convert(Quantity quantity, LengthUnit targetUnit) {
-
-        if (quantity == null || targetUnit == null) {
-            throw new IllegalArgumentException("Invalid input");
-        }
+    public <U extends IMeasurable> Quantity<U> convert(
+            Quantity<U> quantity,
+            U targetUnit
+    ) {
+        Objects.requireNonNull(quantity);
+        Objects.requireNonNull(targetUnit);
 
         return quantity.convertTo(targetUnit);
     }
 
     @Override
-    public Quantity add(Quantity q1, Quantity q2, LengthUnit targetUnit) {
-
-        if (q1 == null || q2 == null || targetUnit == null) {
-            throw new IllegalArgumentException("Invalid input");
-        }
+    public <U extends IMeasurable> Quantity<U> add(
+            Quantity<U> q1,
+            Quantity<U> q2,
+            U targetUnit
+    ) {
+        Objects.requireNonNull(q1);
+        Objects.requireNonNull(q2);
+        Objects.requireNonNull(targetUnit);
 
         return q1.add(q2, targetUnit);
+    }
+
+    @Override
+    public <U extends IMeasurable> boolean areEqual(
+            Quantity<U> q1,
+            Quantity<U> q2
+    ) {
+        Objects.requireNonNull(q1);
+        Objects.requireNonNull(q2);
+
+        return q1.equals(q2);
     }
 }
