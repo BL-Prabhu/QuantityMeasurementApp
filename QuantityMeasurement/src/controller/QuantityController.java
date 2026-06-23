9528fc6cd7d08ff780abd2a841dea2b88dcaab50
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-23 11:45:00 +0530

    FEAT : add Quantity Controller

--- QuantityMeasurement/src/controller/QuantityController.java
+++ QuantityMeasurement/src/controller/QuantityController.java
@@ -1,27 +1,51 @@
 package controller;
 
-import model.LengthUnit;
+
+import model.IMeasurable;
 import model.Quantity;
 import service.QuantityService;
 import service.QuantityServiceImpl;
 
 public class QuantityController {
 
-    private final QuantityService service = new QuantityServiceImpl();
+    private final QuantityService service =
+            new QuantityServiceImpl();
 
-    public Quantity convert(double value, LengthUnit from, LengthUnit to) {
+    public <U extends IMeasurable> Quantity<U> convert(
+            double value,
+            U from,
+            U to
+    ) {
+        Quantity<U> quantity =
+                new Quantity<>(value, from);
 
-        Quantity quantity = new Quantity(value, from);
         return service.convert(quantity, to);
     }
 
-    public Quantity add(double v1, LengthUnit u1,
-                        double v2, LengthUnit u2,
-                        LengthUnit target) {
+    public <U extends IMeasurable> Quantity<U> add(
+            double v1, U u1,
+            double v2, U u2,
+            U target
+    ) {
+        Quantity<U> q1 =
+                new Quantity<>(v1, u1);
 
-        Quantity q1 = new Quantity(v1, u1);
-        Quantity q2 = new Quantity(v2, u2);
+        Quantity<U> q2 =
+                new Quantity<>(v2, u2);
 
         return service.add(q1, q2, target);
+    }
+
+    public <U extends IMeasurable> boolean compare(
+            double v1, U u1,
+            double v2, U u2
+    ) {
+        Quantity<U> q1 =
+                new Quantity<>(v1, u1);
+
+        Quantity<U> q2 =
+                new Quantity<>(v2, u2);
+
+        return service.areEqual(q1, q2);
     }
 }
