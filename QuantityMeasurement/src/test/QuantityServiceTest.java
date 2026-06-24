61ea69608fc55bcbc697ae833fbdd894b242daf5
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-24 11:56:17 +0530

    FEAT : add Quantity Service Test

--- QuantityMeasurement/src/test/QuantityServiceTest.java
+++ QuantityMeasurement/src/test/QuantityServiceTest.java
@@ -1,52 +1,45 @@
 package test;
 
-
 import model.LengthUnit;
 import model.Quantity;
-import org.junit.Test;
 import service.QuantityService;
 import service.QuantityServiceImpl;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class QuantityServiceTest {
 
-    private final QuantityService<LengthUnit> service =
-            new QuantityServiceImpl<>();
-
-    private static final double EPSILON = 0.0001;
+    private final QuantityService service =
+            new QuantityServiceImpl();
 
     @Test
-    public void testSubtraction() {
+    public void testComparisonEqual() {
 
-        Quantity<LengthUnit> result =
-                service.subtract(
-                        new Quantity<>(10, LengthUnit.FEET),
-                        new Quantity<>(6, LengthUnit.INCHES),
-                        LengthUnit.FEET
-                );
+        Quantity<LengthUnit> q1 =
+                new Quantity<>(1, LengthUnit.FEET);
 
-        assertEquals(9.5, result.getValue(), EPSILON);
+        Quantity<LengthUnit> q2 =
+                new Quantity<>(12, LengthUnit.INCHES);
+
+        assertEquals(0, service.compare(q1, q2));
     }
 
     @Test
-    public void testDivision() {
+    public void testSorting() {
 
-        double result =
-                service.divide(
-                        new Quantity<>(10, LengthUnit.FEET),
-                        new Quantity<>(2, LengthUnit.FEET)
-                );
-
-        assertEquals(5.0, result, EPSILON);
-    }
-
-    @Test(expected = ArithmeticException.class)
-    public void testDivisionByZero() {
-
-        service.divide(
-                new Quantity<>(10, LengthUnit.FEET),
-                new Quantity<>(0, LengthUnit.FEET)
+        List<Quantity<LengthUnit>> list = Arrays.asList(
+                new Quantity<>(3, LengthUnit.FEET),
+                new Quantity<>(24, LengthUnit.INCHES),
+                new Quantity<>(1, LengthUnit.YARDS)
         );
+
+        List<Quantity<LengthUnit>> sorted = service.sort(list);
+
+        assertTrue(sorted.get(0).compareTo(sorted.get(1)) <= 0);
     }
 }
