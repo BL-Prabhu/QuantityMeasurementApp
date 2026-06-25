bbaecf1614744b16d56986568eeb23041930badf
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-25 15:06:48 +0530

    FEAT : add Measurable Model

--- QuantityMeasurement/src/controller/QuantityMeasurementController.java
+++ QuantityMeasurement/src/controller/QuantityMeasurementController.java
@@ -1,27 +1,18 @@
 package controller;
 
-import dto.QuantityDTO;
-import entity.QuantityMeasurementEntity;
-import service.IQuantityMeasurementService;
+public class QuantityController {
 
-public class QuantityMeasurementController {
+    private double value;
+    private String unit;
+    private String type; // LENGTH, WEIGHT, VOLUME, TEMPERATURE
 
-    private final IQuantityMeasurementService service;
-
-    public QuantityMeasurementController(IQuantityMeasurementService service) {
-        this.service = service;
+    public QuantityController(double value, String unit, String type) {
+        this.value = value;
+        this.unit = unit;
+        this.type = type;
     }
 
-    public void performAdd(QuantityDTO q1, QuantityDTO q2) {
-        QuantityMeasurementEntity result = service.add(q1, q2);
-        System.out.println(result);
-    }
-
-    public void performSubtract(QuantityDTO q1, QuantityDTO q2) {
-        System.out.println(service.subtract(q1, q2));
-    }
-
-    public void performCompare(QuantityDTO q1, QuantityDTO q2) {
-        System.out.println(service.compare(q1, q2));
-    }
+    public double getValue() { return value; }
+    public String getUnit() { return unit; }
+    public String getType() { return type; }
 }
--- QuantityMeasurement/src/model/IMeasurable.java
+++ QuantityMeasurement/src/model/IMeasurable.java
@@ -9,4 +9,17 @@ public interface IMeasurable {
     double convertFromBaseUnit(double baseValue);
 
     String getUnitName();
+
+    // Default: supports arithmetic
+    default boolean supportsArithmetic() {
+        return true;
+    }
+
+    default void validateOperationSupport(String operation) {
+        if (!supportsArithmetic()) {
+            throw new UnsupportedOperationException(
+                    operation + " not supported for this unit type"
+            );
+        }
+    }
 }
