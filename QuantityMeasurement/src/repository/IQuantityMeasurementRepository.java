759262d1369659a74411d29f53633c2eb418efab
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-25 15:11:36 +0530

    FEAT : add Quantity Measurement Repository

--- QuantityMeasurement/src/repository/IQuantityMeasurementRepository.java
+++ QuantityMeasurement/src/repository/IQuantityMeasurementRepository.java
@@ -1,11 +1,9 @@
 package repository;
 
+
 import entity.QuantityMeasurementEntity;
-import java.util.List;
 
 public interface IQuantityMeasurementRepository {
 
     void save(QuantityMeasurementEntity entity);
-
-    List<QuantityMeasurementEntity> findAll();
 }
