bd8e81ca79cae4ce35e24b0ce9f34522073d9738
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-27 10:31:21 +0530

    FEAT : add Quantity Measurement Repository

---
+++ App/src/main/java/quantitymeasurement/repository/QuantityMeasurementRepository.java
@@ -0,0 +1,27 @@
+package quantitymeasurement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import quantitymeasurement.model.QuantityMeasurementEntity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface QuantityMeasurementRepository extends JpaRepository<QuantityMeasurementEntity, Long> {
+
+    List<QuantityMeasurementEntity> findByOperation(String operation);
+
+    List<QuantityMeasurementEntity> findByThisMeasurementType(String measurementType);
+
+    List<QuantityMeasurementEntity> findByCreatedAtAfter(LocalDateTime date);
+
+    @Query("SELECT q FROM QuantityMeasurementEntity q WHERE q.operation = :operation AND q.isError = false")
+    List<QuantityMeasurementEntity> findSuccessfulOperations(@Param("operation") String operation);
+
+    long countByOperationAndIsErrorFalse(String operation);
+
+    List<QuantityMeasurementEntity> findByIsErrorTrue();
+}
